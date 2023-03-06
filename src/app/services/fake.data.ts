d0a4b4821d1e54a6ca040ace2c8644085fc6f75a
miguelonga <miguelmundiaragones@gmail.com>
2023-03-06 22:28:46 +0100

    extract fake data to new file

---
+++ src/app/services/fake.data.ts
@@ -0,0 +1,56 @@
+export let fakeData = [
+    {
+      id: 1,
+      level: 3.1,
+      clubId: 8,
+      players: [{id:1}],
+      date: Date.now() + 18000000,
+      indoor: true,
+      price: 14
+    },
+    {
+      id: 2,
+      level: 2.1,
+      clubId: 8,
+      players: [{id:1},{id:2},{id:3}],
+      date: Date.now() + 10000000000,
+      indoor: true,
+      price: 11.2
+    },
+    {
+      id: 3,
+      level: 4.3,
+      clubId: 8,
+      players: [{id:1},{id:2}],
+      date: Date.now() + 10200000000,
+      indoor: false,
+      price: 9
+    },
+    {
+      id: 4,
+      level: 1.5,
+      clubId: 8,
+      players: [{id:1}],
+      date: Date.now() + 180000000000,
+      indoor: false,
+      price: 11
+    },
+    {
+      id: 5,
+      level: 3.1,
+      clubId: 8,
+      players: [{id:1}],
+      date: Date.now() + 1802200000,
+      indoor: false,
+      price: 10
+    },
+    {
+      id: 6,
+      level: 4.5,
+      clubId: 10,
+      players: [{id:1},{id:2},{id:3}],
+      date: Date.now() + 18000000 + 18000000 + 18000000,
+      indoor: true,
+      price: 13
+    },
+]
--- src/app/services/matches.service.ts
+++ src/app/services/matches.service.ts
@@ -1,68 +1,12 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
-import { Match } from '../models/match.model';
+import { fakeData } from './fake.data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchesService {
-  fakeData = [
-    {
-      id: 1,
-      level: 3.1,
-      clubId: 8,
-      players: [{id:1}],
-      date: Date.now() + 18000000,
-      indoor: true,
-      price: 14
-    },
-    {
-      id: 2,
-      level: 2.1,
-      clubId: 8,
-      players: [{id:1},{id:2},{id:3}],
-      date: Date.now() + 10000000000,
-      indoor: true,
-      price: 11.2
-    },
-    {
-      id: 3,
-      level: 4.3,
-      clubId: 8,
-      players: [{id:1},{id:2}],
-      date: Date.now() + 10200000000,
-      indoor: false,
-      price: 9
-    },
-    {
-      id: 4,
-      level: 1.5,
-      clubId: 8,
-      players: [{id:1}],
-      date: Date.now() + 180000000000,
-      indoor: false,
-      price: 11
-    },
-    {
-      id: 5,
-      level: 3.1,
-      clubId: 8,
-      players: [{id:1}],
-      date: Date.now() + 1802200000,
-      indoor: false,
-      price: 10
-    },
-    {
-      id: 6,
-      level: 4.5,
-      clubId: 10,
-      players: [{id:1},{id:2},{id:3}],
-      date: Date.now() + 18000000 + 18000000 + 18000000,
-      indoor: true,
-      price: 13
-    },
-  ]
-  
+  fakeData = fakeData
   constructor() {}
 
   getMatches(){
